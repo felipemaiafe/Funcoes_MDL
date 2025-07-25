ed5068cbe98161fb5f435f30b3b1c07238fa4b84
Felipe Maia <139877160+felipemaiafe@users.noreply.github.com>
2025-07-25 15:18:28 -0300

    fix: Simplify resource path handling for better compatibility with PyInstaller

--- db_utils.py
+++ db_utils.py
@@ -6,10 +6,9 @@ import sys
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
+    if getattr(sys, 'frozen', False):
+        base_path = os.path.dirname(sys.executable)
+    else:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
